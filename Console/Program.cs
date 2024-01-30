da50291d8b2f019a969409ca99cd0f4dc9d5ac88
MrDLC7 <mafia78ton@gmail.com>
2024-01-30 22:07:16 +0200

    Fix #2 dev

--- Console/Program.cs
+++ Console/Program.cs
@@ -7,6 +7,15 @@
         Console.ReadKey();
         const int d = 81;
         Console.WriteLine($"Test {d * d}");
+        Test(d);
         Console.ReadKey();
+    }
+    static void Test(int i)
+    {
+        for (int j = 1; j < i; j++)
+        {
+            Console.Write(j + " ");
+            if (j % 20 == 0) Console.WriteLine(",");
+        }
     }
 }
