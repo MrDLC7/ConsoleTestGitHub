fadfcc20a0cb1b5aeaa95f671c4e1265c29e6d3b
MrDLC7 <mafia78ton@gmail.com>
2024-01-30 22:06:00 +0200

    Fix #1

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,1 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Test GitHub!");
+        Console.WriteLine("Test develop!");
+        Console.ReadKey();
+        const int d = 81;
+        Console.WriteLine($"Test {d * d}");
+        Console.ReadKey();
+    }
+}
